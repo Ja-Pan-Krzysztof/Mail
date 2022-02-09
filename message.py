bcb43e13b717f8195700b4c75c19fb001986ec26
Ja <Ja_Pan_Krzysztof@gmail.com>
2022-02-09 14:52:09 +0100

    Dodanie templates | html'a

---
+++ message.py
@@ -0,0 +1,13 @@
+import jinja2
+
+
+class Message:
+    TEMPLATE_FILE = './templates/wow.html'
+
+    def __init__(self):
+        template_loader = jinja2.FileSystemLoader(searchpath='./')
+        self.template_env = jinja2.Environment(loader=template_loader)
+        self.template = self.template_env.get_template(self.TEMPLATE_FILE)
+
+    def render(self, **kwargs):
+        return self.template.render(**kwargs)
